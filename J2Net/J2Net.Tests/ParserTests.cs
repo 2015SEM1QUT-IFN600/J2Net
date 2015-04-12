c7094b7b40f5dba4814dc2f7f157105f7e8d1ce5
Jason Brown <jasbro@live.com>
2015-04-12 18:03:06 +1000

    Parser Test Framework Added

--- J2Net/J2Net.Tests/ParserTests.cs
+++ J2Net/J2Net.Tests/ParserTests.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Antlr4.Runtime;
+using Antlr4.Runtime.Tree;
+using Antlr4.Runtime.Tree.Pattern;
 using J2Net.Grammar;
 
 namespace J2Net.Tests
@@ -8,28 +10,50 @@ namespace J2Net.Tests
     [TestClass]
     public class ParserTests
     {
-        private static void AssertRule(String javaCode)
+        static JavaLexer _Lexer;
+        static JavaParser _Parser;
+
+        private static ParseTreeMatch GetParseTreeMatch(String testCode, String pattern, String ruleName)
         {
-            AntlrInputStream antlrStream = new AntlrInputStream(javaCode);
-            JavaLexer lexer = new JavaLexer(antlrStream);
-            CommonTokenStream token = new CommonTokenStream(lexer);
+            //Generate a lexer and parser based on test code
+            JavaLexer lexer = new JavaLexer(new AntlrInputStream(testCode));
+            JavaParser parser = new JavaParser(new CommonTokenStream(lexer));
 
-            JavaParser parser = new JavaParser(token);
+            //use .Net Reflection to get the Paser's Rule's method and execute it to generate a Parse Tree object
+            System.Reflection.MethodInfo ruleMethod = parser.GetType().GetMethod(ruleName);
+            IParseTree ruleTree = (IParseTree)ruleMethod.Invoke(parser, null);
 
-            // Option 1: implement IJavaListner, then build a test for each override
-            //http://stackoverflow.com/questions/15050137/once-grammar-is-complete-whats-the-best-way-to-walk-an-antlr-v4-tree
+            //From our empty Lexer and Parser, compile a ParseTreePattern 
+            ParseTreePattern treePattern = GetParseTreePattern(pattern, ruleName);
 
-            // Option 2: throw errors from inside the rule, and catch in test
-            // http://meri-stuff.blogspot.com.au/2011/08/antlr-tutorial-hello-word.html#ChangingCatchinParser
-            //   another example of same technique, but implemented differently
-            // http://meri-stuff.blogspot.com.au/2011/08/antlr-tutorial-hello-word.html#ChangingCatchinParser
-
-            
+            //Pass the Rule's tree into the tree Pattern to get any match
+            return treePattern.Match(ruleTree);
         }
 
-        [TestMethod]
-        public void TestMethod1()
+        private static ParseTreePattern GetParseTreePattern(String pattern, String ruleName)
         {
+            if (_Lexer == null)
+            {
+                _Lexer = new JavaLexer(null);
+                _Parser = new JavaParser(new CommonTokenStream(_Lexer));
+            }
+            return _Parser.CompileParseTreePattern(pattern, _Parser.GetRuleIndex(ruleName));
+        }
+
+
+        [TestMethod]
+        public void Parser_Types_type()
+        {
+            ParseTreeMatch match;
+            match = GetParseTreeMatch("int", "<a:integralType>", "type");
+            Assert.AreEqual(match.Get("a").GetText(), "int");
+
+            //this is obviously a wrong test, because typeName rule is broken at this time. But this gives an 
+            // example of using multiple Pattern tags and going through Parser rules
+            match = GetParseTreeMatch("@. long", "<b:annotation> <a:integralType>", "type");
+            Assert.AreEqual(match.Get("a").GetText(), "long");
+            Assert.AreEqual(match.Get("b").GetText(), "@.");
+
         }
     }
 }
