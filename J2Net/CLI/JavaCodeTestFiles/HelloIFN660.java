3d2d6e91083b1f7508c75479a7220814f84f5894
Jason Brown <jasbro@live.com>
2015-04-24 16:43:33 +1000

    wayne's code

--- J2Net/CLI/JavaCodeTestFiles/HelloIFN660.java
+++ J2Net/CLI/JavaCodeTestFiles/HelloIFN660.java
@@ -1,11 +1,9 @@
 package com.company;
 
 public class Main {
-    static int j;
 
     public static void main(String[] args) {
         int i;
-        i = 0;
-        j = i + 660;
+        i = 42;
     }
 }
