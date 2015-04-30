0fadad7b8f46580455ff70399d0f17e341095acf
Jason Brown <jasbro@live.com>
2015-04-30 11:06:21 +1000

    NameBinder Squad structure setup

---
+++ J2Net/J2Net/J2NetNameBinder.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using J2Net.Grammar;
+
+namespace J2Net
+{
+    class J2NetNameBinder : JavaBaseListener
+    {
+        //TODO: Identify which rules to push declarations onto the Symbol Table
+
+        //TODO: Push items onto Symbol Table
+    }
+}
---
+++ J2Net/J2Net/ScopeStack.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace J2Net
+{
+    class ScopeStack
+    {
+        //TODO: Find out what kind of data type this should be to accomplish a Symbol Table
+
+        //TODO: build this as that data type 
+    }
+}
